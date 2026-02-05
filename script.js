e3712627e43736812e41067129bbc97589a7c850
ShriramMapalakaje <108290635+ShriramMapalakaje@users.noreply.github.com>
2026-02-05 15:18:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function volume_sphere() {
-    //Write your code here
+    
+    event.preventDefault();
+
+    const radiusInput = document.getElementById('radius').value;
+    const volumeField = document.getElementById('volume');
+    
   
-} 
+    const r = parseFloat(radiusInput);
+
+
+    if (isNaN(r) || r < 0) {
+        volumeField.value = 'NaN';
+    } else {
+       
+        const volume = (4 / 3) * Math.PI * Math.pow(r, 3);
+        
+       
+        volumeField.value = volume.toFixed(4);
+    }
+}
+
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
